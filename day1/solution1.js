68abfc44cb119d791c188f56951d983a553fe3c0
TanishqSingla <tanishqsingla00@gmail.com>
2024-12-05 23:41:43 +0530

    feat: add solution to part2

--- day1/solution1.js
+++ day1/solution1.js
@@ -1,11 +1,11 @@
 const decoder = new TextDecoder("utf8");
-const input = await Deno.readFile("input.txt");
+const input = Deno.readFileSync("input.txt");
 
 const data = decoder.decode(input);
 
-const lists = data.split('\n');
-const firstList = []
-const secondList = []
+const lists = data.split("\n");
+const firstList = [];
+const secondList = [];
 
 for (const line of lists) {
 	const inputs = line.split(/\s+/);
@@ -14,9 +14,25 @@ for (const line of lists) {
 	secondList.push(+inputs[1]);
 }
 
-firstList.sort((a,b) => a - b);
-secondList.sort((a,b) => a - b);
+firstList.sort((a, b) => a - b);
+secondList.sort((a, b) => a - b);
 
 let distance = 0;
-firstList.forEach((item, index) => {distance += Math.abs(item - secondList[index])});
+
+for (let i = 0; i < firstList.length; i++) {
+	distance += Math.abs(firstList[i] - secondList[i]);
+}
+
 console.log(distance);
+
+const count = {};
+for (const item of secondList) {
+	if (!count[item]) count[item] = 1;
+	else count[item]++;
+}
+
+let score = 0;
+for (const item of firstList) {
+	if (count[item]) score += item * count[item];
+}
+console.log(score);
