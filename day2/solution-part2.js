24335d94b707124bcf346970e0152ed881c35dd8
TanishqSingla <tanishqsingla00@gmail.com>
2024-12-18 20:20:52 +0530

    feat: add day2 part 2 solution

--- day2/solution-part2.js
+++ day2/solution-part2.js
@@ -1,4 +1,4 @@
-// Part 1
+// Part 2
 
 import { getInput } from "../utils/utils.js";
 
@@ -10,26 +10,37 @@ lines.pop();
 
 let safeReportsCount = 0;
 
-for (const line of lines) {
-	const numbers = line.trim().split(/\s+/);
+const isSafe = (numbers) => {
 
 	let previous;
 	let isIncreasing;
 	let safe = true;
 	for (const number of numbers) {
 		if (typeof previous === 'undefined') { previous = +number; continue }
-		
+
 		const difference = (+number) - previous;
 		if (Math.abs(difference) > 3 || difference == 0) { safe = false; break; }
 
-		if(typeof isIncreasing === 'undefined') {isIncreasing = difference < 0 ? -1 : 1;};
+		if (typeof isIncreasing === 'undefined') { isIncreasing = difference < 0 ? -1 : 1; };
 
 		if ((isIncreasing == 1 && difference < 0) || (isIncreasing == -1 && difference > 0)) { safe = false; break; }
 
 		previous = +number;
 	}
 
-	if (safe) { safeReportsCount++; }
+	return safe;
+}
+
+for (const line of lines) {
+	const numbers = line.trim().split(/\s+/);
+
+	if (isSafe(numbers)) {safeReportsCount++; continue; };
+
+	for(let i = 0; i < numbers.length; i++) {
+		const temp = numbers.slice();
+		temp.splice(i,1);
+		if (isSafe(temp)) { safeReportsCount++; break; }
+	}
 }
 
 console.log(safeReportsCount)
